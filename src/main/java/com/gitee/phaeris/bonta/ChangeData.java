57c3521e0c589070007656a935b49ab41f8708a6
wangyahui <wangyahui@weihaizixun.com>
2023-10-18 11:48:12 +0800

    changeData source增加注释

--- src/main/java/com/gitee/phaeris/bonta/ChangeData.java
+++ src/main/java/com/gitee/phaeris/bonta/ChangeData.java
@@ -40,28 +40,64 @@ public class ChangeData {
     @Data
     public static class Source {
 
+        /**
+         * 源数据库的名称
+         */
         private String db;
 
+        /**
+         * 源表的名称
+         */
         private String table;
 
+        /**
+         * 源自的 binlog 或 log 文件的名称
+         */
         private String file;
 
+        /**
+         * 连接器的类型（例如，"mysql"、"postgres"）
+         */
         private String connector;
 
+        /**
+         * Debezium 连接器配置的名称
+         */
         private String name;
 
+        /**
+         * 变更发生的 binlog 或 log 文件中的位置
+         */
         private Long pos;
 
+        /**
+         * 变更在 binlog 文件中的行偏移位置
+         */
         private Integer row;
 
+        /**
+         * 如果可用，全局事务标识符 (GTID)
+         */
         private String gtid;
 
+        /**
+         * 生成事件的 MySQL 服务器的唯一标识
+         */
         private String server_id;
 
+        /**
+         * Debezium 连接器的版本
+         */
         private String version;
 
+        /**
+         * 事件发生的时间戳（毫秒）
+         */
         private Long ts_ms;
 
+        /**
+         * 表示事件是否属于快照（"true" 或 "false")
+         */
         private String snapshot;
     }
 }
